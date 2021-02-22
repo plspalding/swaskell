b46445e2cc4bfced1d8a1e6fa24c2c8c9fb32c48
Preston Spalding <gsnpreston@gmail.com>
2021-02-22 21:37:13 +0000

    Create project

---
+++ Swaskell/Swaskell/Swaskell.h
@@ -0,0 +1,18 @@
+//
+//  Swaskell.h
+//  Swaskell
+//
+//  Created by Preston Spalding on 22/02/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Swaskell.
+FOUNDATION_EXPORT double SwaskellVersionNumber;
+
+//! Project version string for Swaskell.
+FOUNDATION_EXPORT const unsigned char SwaskellVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swaskell/PublicHeader.h>
+
+
